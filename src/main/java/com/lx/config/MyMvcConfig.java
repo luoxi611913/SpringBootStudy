f7d54faa10a3abc9a9bd2b6548061b953f0c9f2f
luoxi611913 <729697363@qq.com>
2020-10-30 01:35:37 +0800

    20201030 博客代码追加

---
+++ src/main/java/com/lx/config/MyMvcConfig.java
@@ -0,0 +1,25 @@
+package com.lx.config;
+
+import com.lx.intercepters.LoginInterCepter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/ddd").setViewName("index");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoginInterCepter()).addPathPatterns("/**").excludePathPatterns("/");
+    }
+}
